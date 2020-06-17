37a3143bd3c65a5489296797e27336b84dd62f61
Richard Thorek <richard@thorek.net>
2020-06-17 19:22:51 +1000

    add files from storage to repo

---
+++ site/site/index.js
@@ -0,0 +1,13 @@
+
+
+
+// Sidebar (Collaspse)
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
